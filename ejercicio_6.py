c3c17d92186a907e2ccb478393b86ee8aabe45ca
AARON HENAO <aaronhenao27@gmail.com>
2023-02-16 09:09:19 -0500

    Ejercicio 6

---
+++ ejercicio_6.py
@@ -0,0 +1,10 @@
+class Carta():
+
+    def __init__(self, valor: int, pinta: str):
+        self.valor = valor
+        self.pinta = pinta
+        self.pinta_1 = "Copas"
+        self.pinta_2 = "Oros"
+        self.pinta_3 = "Bastos"
+        self.pinta_4 = "Espadas"
+
